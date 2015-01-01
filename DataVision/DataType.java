5fb554b8f288af7c7fd5c7f31b70cbbb01a0f192
tarma <tarma_fu@hotmail.com>
2015-01-01 16:02:31 +0800

    DataVision v0.1

---
+++ DataVision/DataType.java
@@ -0,0 +1,13 @@
+
+public class DataType {
+
+	public int temp;
+	public int humid;
+	public int light;
+
+	public DataType(int temp, int humid, int light) {
+		this.temp = temp;
+		this.humid = humid;
+		this.light = light;
+	}
+}
---
+++ DataVision/README.md
@@ -0,0 +1,8 @@
+HOW TO RUN:
+1. Open terminal and change to this directory.
+2. make
+3. ./run
+
+Before you run this program, you need to launch SerialForwarder.
+
+tarma
