e5b4c3b96965d8cb4e22eb64e8a979bbb93889bb
tarma <tarma_fu@hotmail.com>
2015-01-02 21:05:09 +0800

    counter from int to long

--- DataVision/SenseMote.h
+++ DataVision/SenseMote.h
@@ -9,13 +9,18 @@ enum {
 
 typedef nx_struct RADIO_MSG {
   nx_uint16_t nodeid;
-  nx_uint16_t counter;
+  nx_uint32_t counter;
   nx_uint16_t time_period;
   nx_uint16_t temperature;
   nx_uint16_t humidity;
   nx_uint16_t light;
   nx_uint32_t total_time;
 } RADIO_MSG;
+
+typedef nx_struct ACK_MSG{
+  nx_uint16_t nodeid;
+  nx_uint32_t counter;
+}ACK_MSG;
 
 typedef nx_struct TIME_MSG {
   nx_uint16_t nodeid;
